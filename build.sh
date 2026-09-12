36fc18da9df4f28e1b3979d1aaf545909a8ce48a
MOSTEFA <rym222@yahoo.fr>
2026-09-12 08:45:51 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,9 +14,12 @@ sudo apt-get install -y bc bison build-essential ccache flex glibc-source libelf
 
 cd $GITHUB_WORKSPACE
 
-echo "=== Clonage du kernel ==="
-git clone https://github.com/LineageOS/android_kernel_motorola_sm8250.git -b lineage-23.2 --depth=1 kernel_sources
+# ==================== 1. CLONAGE DU NOYAU ====================
+echo "=== Clonage du kernel depuis le fork Albanel22 ==="
+git clone --depth=1 --branch kiev-kernelsu-susfs https://github.com/Albanel22/android_kernel_motorola_sm8250.git kernel_sources
 cd kernel_sources
+git log --oneline -1
+cd "$GITHUB_WORKSPACE"
 
 echo "=== Intégration ReSukiSU ==="
 rm -rf drivers/kernelsu kernelSU susfs4ksu || true
